c541251bfffaed0593e10c7bd100b460d50d04c0
Byabasaija <basaijapascal9@gmail.com>
2021-12-16 11:51:04 +0300

    make first api call

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,14 +1,23 @@
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 export const Articles = () => {
-
-
-	
-
+ const apiKey = "ced2ef19151942fb940c489b0737a663" 
+const [articles, setArticles] = useState([])
+	useEffect(() => {
+		async function fetchRecents() {
+			const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-11-16&sortBy=publishedAt&apiKey=ced2ef19151942fb940c489b0737a663') 
+			const fetchedArticles = await response.json(response.data);
+			console.log(response);
+			setArticles(fetchedArticles);
+		}
+		fetchRecents();
+	}, [])
 	return (
 		<>
 		<h1>Articles</h1>
+{/* 		 
+		 { articles && articles.map(article => <div>{article.}</div>} */}
 		
 		</>
 	);
